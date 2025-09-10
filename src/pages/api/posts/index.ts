bd45517c3b567b7d54c7f477d7f80305b08836fc
Abrham Tiliksew <abrham.tilksew@aastustudent.edu.et>
2025-09-10 08:25:39 +0300

    fix registering user shcema

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../../lib/dbConnect';
 import Post from '../../../models/Post';
+import User from '../../../models/User'; // <-- Added this import
 import { jwtVerify, JWTPayload } from 'jose';
 
 const JWT_SECRET = new TextEncoder().encode(
